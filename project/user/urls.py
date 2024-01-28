a6a1b4d10a504476fb217e922a1844c75cdcda17
Yippine <Olange.stone@gmail.com>
2024-01-28 15:25:59 +0800

    fix, feat(front-end): 修復 mystorybooks 頁文字偏移 | 建立新舊兩種前端的設計環境

--- project/user/urls.py
+++ project/user/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('userinfo/', views.user_info, name='user_info'),
     path('userinfo1/', views.user_info_1, name='user_info_1'),
     path('userinfo2/', views.user_info_2, name='user_info_2'),
+    path('loginnew/', views.login_new, name='login_new'),
+    path('registernew/', views.register_new, name='register_new'),
+    path('userinfonew/', views.user_info_new, name='user_info_new'),
+    path('userinfo1new/', views.user_info_1_new, name='user_info_1_new'),
+    path('userinfo2new/', views.user_info_2_new, name='user_info_2_new'),
 ]
