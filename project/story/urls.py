6f3c347dbf055d97bb7aebe0d7ee32753e2536ed
Yippine <Olange.stone@gmail.com>
2024-01-29 20:47:54 +0800

    refactor, fix, feat(front-end): 修復項目相關頁面 — 未正確導向、無法正常顯示 | 優化連接資料庫的項目頁面的版面和樣式 | 分類 urls.py 所有網址

--- project/story/urls.py
+++ project/story/urls.py
@@ -2,28 +2,37 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 一般網頁
     path('createstory/', views.create_story, name='menu/create_story'),
-    path('itemdetails/', views.item_details, name='menu/item_details'),
-    path('itemdetails2/', views.item_details2, name='item_details2'),
-    path('selectitem/', views.select_item, name='menu/select_item'),
-    path('selectitem2/', views.select_item2, name='menu/select_item2'),
-    path('mainroledetails/', views.main_role_details, name='menu/main_role_details'),
-    path('suproledetails/', views.sup_role_details, name='menu/sup_role_details'),
     path('selectmainrole/', views.select_main_role, name='menu/select_main_role'),
     path('selectsuprole/', views.select_sup_role, name='menu/select_sup_role'),
+    path('selectitem/', views.select_item, name='menu/select_item'),
+    path('mainroledetails/', views.main_role_details, name='menu/main_role_details'),
+    path('suproledetails/', views.sup_role_details, name='menu/sup_role_details'),
+    path('itemdetails/', views.item_details, name='menu/item_details'),
     path('loading/', views.loading, name='display/loading'),
-    path('mystorybooks/', views.my_storybooks, name='display/my_storybooks'),
-    path('socialfeatures/', views.social_features, name='display/social_features'),
     path('storybookdisplay/', views.storybook_display, name='display/storybook_display'),
+    path('socialfeatures/', views.social_features, name='display/social_features'),
+    path('mystorybooks/', views.my_storybooks, name='display/my_storybooks'),
+
+    # 新版網頁
     path('createstorynew/', views.create_story_new, name='menu/create_story_new'),
-    path('itemdetailsnew/', views.item_details_new, name='menu/item_details_new'),
+    path('selectmainrolenew/', views.select_main_role_new, name='menu/select_main_role_new'),
+    path('selectsuprolenew/', views.select_sup_role_new, name='menu/select_sup_role_new'),
     path('selectitemnew/', views.select_item_new, name='menu/select_item_new'),
     path('mainroledetailsnew/', views.main_role_details_new, name='menu/main_role_details_new'),
     path('suproledetailsnew/', views.sup_role_details_new, name='menu/sup_role_details_new'),
-    path('selectmainrolenew/', views.select_main_role_new, name='menu/select_main_role_new'),
-    path('selectsuprolenew/', views.select_sup_role_new, name='menu/select_sup_role_new'),
+    path('itemdetailsnew/', views.item_details_new, name='menu/item_details_new'),
     path('loadingnew/', views.loading_new, name='display/loading_new'),
-    path('mystorybooksnew/', views.my_storybooks_new, name='display/my_storybooks_new'),
-    path('socialfeaturesnew/', views.social_features_new, name='display/social_features_new'),
     path('storybookdisplaynew/', views.storybook_display_new, name='display/storybook_display_new'),
+    path('socialfeaturesnew/', views.social_features_new, name='display/social_features_new'),
+    path('mystorybooksnew/', views.my_storybooks_new, name='display/my_storybooks_new'),
+
+    # 連接資料庫
+    path('selectmainrolebydata/', views.select_main_role_by_data, name='menu/select_main_role_by_data'),
+    path('selectsuprolebydata/', views.select_sup_role_by_data, name='menu/select_sup_role_by_data'),
+    path('selectitembydata/', views.select_item_by_data, name='menu/select_item_by_data'),
+    path('mainroledetailsbydata/<int:item_id>/', views.main_role_details_by_data, name='menu/main_role_details_by_data'),
+    path('suproledetailsbydata/<int:item_id>/', views.sup_role_details_by_data, name='menu/sup_role_details_by_data'),
+    path('itemdetailsbydata/<int:item_id>/', views.item_details_by_data, name='menu/item_details_by_data'),
 ]
