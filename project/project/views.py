93d8684792643f072e91e27c016088c561500411
beilerwk <wkmimi240088@gmail.com>
2024-02-04 14:50:51 +0800

    feat(front-end): home首頁響應、縮放優化

--- project/project/views.py
+++ project/project/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 
+
+#使用 redirect 函數將請求重定向到 "/home"。
 def redirect_to_home(request):
     return redirect("/home")
 
+
+#render 函數將 home.html 模板渲染為HTTP響應
 def home(request):
     return render(request, "home.html")
 
