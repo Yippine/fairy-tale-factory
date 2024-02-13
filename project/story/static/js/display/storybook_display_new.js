810e2a301727156a7fc8cddfbc38ad111cd1c110
Yippine <Olange.stone@gmail.com>
2024-02-13 13:06:02 +0800

    feat(front-end): 【storybookdisplaynew 頁】建立上下頁的導頁邏輯 — 建立上下頁按鈕 | 建立左右鍵事件 | 調整優化過後的 js

--- project/story/static/js/display/storybook_display_new.js
+++ project/story/static/js/display/storybook_display_new.js
@@ -1,7 +1,12 @@
+const maxPage = 20;
+
 document.addEventListener("DOMContentLoaded", () => {
     adjustStoryAlignment();
-    setupPageNavigation();
-    setupPopupHandlers();
+    const urlParams = new URLSearchParams(window.location.search);
+    let page = parseInt(urlParams.get("page"), 10);
+    initializePageNavigation(page);
+    setupNavigationButtons(page);
+    // setupPopupHandlers();
 });
 
 window.addEventListener("resize", adjustStoryAlignment);
@@ -11,24 +16,41 @@ function adjustStoryAlignment() {
     storyContainer.style.alignItems = storyContainer.scrollHeight > storyContainer.clientHeight ? "flex-start" : "center";
 }
 
-function setupPageNavigation() {
-    const urlParams = new URLSearchParams(window.location.search);
-    let page = parseInt(urlParams.get("page"), 10);
-    page = isNaN(page) ? 1 : Math.min(Math.max(page, 1), 20);
-    document.getElementById("page").innerHTML = `<span>第 ${page} 頁</span>`;
+function initializePageNavigation(pagePar) {
+    let page = isNaN(pagePar) ? 1 : Math.min(Math.max(pagePar, 1), maxPage);
+    if (page !== pagePar) {
+        window.location.href = `/story/storybookdisplay?page=${page}`;
+    }
+    document.getElementById("page").innerHTML = `<span>第 ${page} 頁 / 共 ${maxPage} 頁</span>`;
+}
 
-    document.querySelectorAll(".group-12-bsJ, .group-13-Tx8").forEach((element) => {
-        element.addEventListener("click", () => navigatePage(element.classList.contains("group-12-bsJ"), page));
+function setupNavigationButtons(page) {
+    const prevPage = document.querySelector(".navigation_button.prev");
+    const nextPage = document.querySelector(".navigation_button.next");
+    if (page === 1) {
+        prevPage.style.display = "none";
+    } else if (page === maxPage) {
+        nextPage.style.display = "none";
+    }
+    prevPage.addEventListener("click", () => navigatePage(false, page));
+    nextPage.addEventListener("click", () => navigatePage(true, page));
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === "ArrowLeft" && prevPage.style.display != "none") {
+            navigatePage(false, page);
+        } else if (event.key === "ArrowRight" && nextPage.style.display != "none") {
+            navigatePage(true, page);
+        }
     });
 }
 
 function navigatePage(isNext, currentPage) {
-    const newPage = isNext ? Math.min(currentPage + 1, 20) : Math.max(currentPage - 1, 1);
+    const newPage = isNext ? Math.min(currentPage + 1, maxPage) : Math.max(currentPage - 1, 1);
     window.location.href = `/story/storybookdisplay?page=${newPage}`;
 }
 
 function setupPopupHandlers() {
-    ["save", "home"].forEach((type) => {
+    ["save_story", "go_home"].forEach((type) => {
         const button = document.querySelector(`#${type}_button`);
         const popup = document.querySelector(`#${type}_pop_up`);
         button.addEventListener("click", (event) => togglePopup(popup, event));
@@ -40,9 +62,9 @@ function setupPopupHandlers() {
         }
     });
 
-    document.getElementById("check_save_button").addEventListener("click", () => {
+    document.getElementById("confirm_button").addEventListener("click", () => {
         document.querySelector("#save_pop_up").style.display = "none";
-        showSaveSuccessfulPopup();
+        showSaveSuccessPopup();
     });
 }
 
@@ -53,10 +75,10 @@ function togglePopup(popup, event) {
     popup.style.display = isVisible ? "none" : "flex";
 }
 
-function showSaveSuccessfulPopup() {
-    const saveSuccessfulPopUp = document.querySelector("#save_successful_pop_up");
-    saveSuccessfulPopUp.style.display = "flex";
+function showSaveSuccessPopup() {
+    const saveSuccessPopup = document.querySelector("#save_success_pop_up");
+    saveSuccessPopup.style.display = "flex";
     setTimeout(() => {
-        saveSuccessfulPopUp.style.display = "none";
+        saveSuccessPopup.style.display = "none";
     }, 1500);
 }
